f4f41c306eb25e1f917fa7c5d19262d4d58fff6f
Tyler M. <tjmcdowell726@gmail.com>
2024-10-22 11:00:35 -0400

    adjusted js

--- script.js
+++ script.js
@@ -2,6 +2,59 @@ const quizContainer = document.getElementById('quiz');
 const scoreContainer = document.getElementById('score');
 const submitButton = document.getElementById('submit');
 
+const questions = [
+    {
+        question: "Who Wrote the Declaration of Independence?",
+        answers: {
+            a: "Thomas Jefferson",
+            b: "Alexander Hamilton",
+            c: "Robert R. Livingston",
+            d: "George Washington",
+    
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Who was George Washington's Vice President?",
+        answers: {
+            a:"Alexander Hamilton",
+            b:"John Hancock",
+            c:"Thomas Jefferson",
+            d: "John Adams",
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Which of the first four presidents never went to college?",
+        answers: {
+            a:"John Adams",
+            b:"George Washington",
+            c:"Thomas Jefferson",
+            d: "James Madison",
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "When did the Founders sign the Declaration of Independence?",
+        answers: {
+            a:"July 2, 1776",
+            b:"August 2, 1776",
+            c:"July 4, 1776",
+            d: "August 6, 1776",
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Who was called the 'Father of the Constitution'? ",
+        answers: {
+            a:"Alexander Hamilton",
+            b:"Thomas Jefferson",
+            c:"James Madison",
+            d: "John Adams",
+        },
+        correctAnswer: "c"
+    },
+    ];
 
 function makeQuiz() {
     const output = [];
@@ -55,69 +108,6 @@ submitButton.addEventListener('click', (event) => {
     }
 });
 document.addEventListener('DOMContentLoaded', makeQuiz);
-
-const questions = [
-{
-    question: "Who Wrote the Declaration of Independence?",
-    answers: {
-        a: "Thomas Jefferson",
-        b: "Alexander Hamilton",
-        c: "Robert R. Livingston",
-        d: "George Washington",
-
-    },
-    correctAnswer: "a"
-},
-
-{
-    question: "Who was George Washington's Vice President?",
-    answers: {
-        a:"Alexander Hamilton",
-        b:"John Hancock",
-        c:"Thomas Jefferson",
-        d: "John Adams",
-    },
-    correctAnswer: "d"
-},
-
-
-{
-    question: "Which of the first four presidents never went to college?",
-    answers: {
-        a:"John Adams",
-        b:"George Washington",
-        c:"Thomas Jefferson",
-        d: "James Madison",
-    },
-    correctAnswer: "b"
-},
-
-
-{
-    question: "When did the Founders sign the Declaration of Independence?",
-    answers: {
-        a:"July 2, 1776",
-        b:"August 2, 1776",
-        c:"July 4, 1776",
-        d: "August 6, 1776",
-    },
-    correctAnswer: "b"
-},
-
-
-{
-    question: "Who was called the 'Father of the Constitution'? ",
-    answers: {
-        a:"Alexander Hamilton",
-        b:"Thomas Jefferson",
-        c:"James Madison",
-        d: "John Adams",
-    },
-    correctAnswer: "c"
-},
-
-
-];
 
 function reloadPage() {
     location.reload();
