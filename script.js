b61c0e2c3b0c5d0e22c69d804e696b4d4d7ee3d5
RyanS <ryanweeney157@gmail.com>
2024-10-22 08:13:36 -0400

    made javascript array

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 let questions = [
     {}
 ]
+
+const Questions = [
+{
+    question: "Who Wrote the Declaration of Independence?",
+    answers: {
+        a: "Thomas Jefferson",
+        b: "Alexander Hamilton",
+        c: "Robert R. Livingston",
+        d: "George Washington",
+
+    },
+    correctAnswer: "a"
+},
+
+{
+    question: "Who was George Washington's Vice President?",
+    answers: {
+        a:"Alexander Hamilton",
+        b:"John Hancock",
+        c:"Thomas Jefferson",
+        d: "John Adams",
+    },
+    correctAnswer: "d"
+},
+
+];
