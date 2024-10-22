1d81f85e522ae44ad81eb1040344012e663de426
Tyler M. <tjmcdowell726@gmail.com>
2024-10-22 07:57:52 -0400

    generated questions and started final score function

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 let questions = [
-    {}
+    {question: "Blah", answer: "Blah"},
+    {question: "Blah", answer: "Blah"},
+    {question: "Blah", answer: "Blah"},
+    {question: "Blah", answer: "Blah"},
+    {question: "Blah", answer: "Blah"},
+    {question: "Blah", answer: "Blah"},
+    {question: "Blah", answer: "Blah"},
+    {question: "Blah", answer: "Blah"},
+    {question: "Blah", answer: "Blah"},
+    {question: "Blah", answer: "Blah"}
 ]
+let score = 0;
+let scoreElement = document.getElementsById("score");
+
+
+function makeQuestion() {
+
+}
+
+function showCompletedScore() {
+    let finalScore = 
+}
